e1d481ef3563fb1602da3f4b98754170edc0d729
Hermerson Araújo <ne.hermerson@gmail.com>
2024-06-12 22:02:56 -0300

    chore(core): update

---
+++ applications/core/src/modules/subscription/app/SubcriptionRepository.ts
@@ -0,0 +1,11 @@
+import PaginationOptions from "@share/PaginationOptions";
+import Subscription from "@subscription/domain/Subscription";
+import SubscriptionPlan from "@subscription/domain/SubscriptionPlan";
+
+export default interface SubscriptionRepository {
+  createSubscription(subscription: Subscription): Promise<void>;
+  updateSubscription(subscription: Subscription): Promise<void>;
+  createSubscriptionPlan(subscription_plan: SubscriptionPlan): Promise<void>;
+  getSubscriptionById(id: string): Promise<Subscription | null>;
+  getSubscriptions(pagination: PaginationOptions): Promise<Array<Subscription>>;
+}

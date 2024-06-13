4a0338eda57d587d6da29fd61e831878598504c9
Hermerson Araújo <ne.hermerson@gmail.com>
2024-06-12 21:55:26 -0300

    refactor(core): improve subscriber design

--- applications/core/src/modules/subscriber/domain/Subscription.ts
+++ applications/core/src/modules/subscriber/domain/Subscription.ts
@@ -1,39 +1,30 @@
 import Aggregate from "@share/Aggregate";
-import SubscriptionItem, { SubscriptionItemObject } from "./SubscriptionItem";
 
 export type SubscriptionObject = {
   id?: string;
   amount: number;
-  items: Array<SubscriptionItemObject>;
+  recurrence_type: string;
   started_at: Date;
 };
 
 export default class Subscription extends Aggregate<SubscriptionObject> {
   #amount: number;
-  #items: Array<SubscriptionItem> = [];
   #started_at: Date;
+  #recurrence_type: string;
 
   constructor(obj: SubscriptionObject) {
     super(obj.id);
     this.#amount = obj.amount;
     this.#started_at = obj.started_at;
-    for (let idx = 0; idx < obj.items.length; idx++) {
-      this.#items.push(new SubscriptionItem(obj.items[idx]));
-    }
+    this.#recurrence_type = obj.recurrence_type;
   }
 
   toObject(): Required<SubscriptionObject> {
-    const items = [];
-
-    for (let idx = 0; idx < this.#items.length; idx++) {
-      items.push(this.#items[idx].toObject());
-    }
-
     return {
       id: this.id,
       amount: this.#amount,
       started_at: this.#started_at,
-      items,
-    }
+      recurrence_type: this.#recurrence_type,
+    };
   }
 }

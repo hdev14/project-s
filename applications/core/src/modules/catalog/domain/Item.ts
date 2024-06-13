5adcbdf15000f1430e5ca439d9a30b598baee2f9
Hermerson Araújo <ne.hermerson@gmail.com>
2024-06-12 21:55:58 -0300

    chore(core): add some new ideas of domain entities

---
+++ applications/core/src/modules/catalog/domain/Item.ts
@@ -0,0 +1,30 @@
+import Aggregate from "@share/Aggregate";
+
+export type ItemObject = {
+  id?: string;
+  name: string;
+  description: string;
+  is_service: boolean;
+};
+
+export default class Item extends Aggregate<ItemObject> {
+  #name: string;
+  #description: string;
+  #is_service: boolean;
+
+  constructor(obj: ItemObject) {
+    super(obj.id);
+    this.#name = obj.name;
+    this.#description = obj.description;
+    this.#is_service = obj.is_service;
+  }
+
+  toObject(): Required<ItemObject> {
+    return {
+      id: this.id,
+      name: this.#name,
+      description: this.#description,
+      is_service: this.#is_service,
+    };
+  }
+}

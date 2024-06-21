9bad67188cad9394ac5bc833665f966941f6c965
Hermerson Araújo <ne.hermerson@gmail.com>
2024-06-21 16:15:15 -0300

    test: create test to ensure that modules don't have cyclic deps

--- applications/core/src/modules/architecture_boundary.arch.test.ts
+++ applications/core/src/modules/architecture_boundary.arch.test.ts
@@ -34,4 +34,43 @@ describe("Architecture Boundaries", () => {
 
     await expect(rule).toPassAsync()
   });
+
+  it('should not have cyclic dependency between modules', () => {
+    const auth_rule = filesOfProject()
+      .inFolder('auth')
+      .should()
+      .beFreeOfCycles();
+
+    const catalog_rule = filesOfProject()
+      .inFolder('catalog')
+      .should()
+      .beFreeOfCycles();
+
+    const company_rule = filesOfProject()
+      .inFolder('company')
+      .should()
+      .beFreeOfCycles();
+
+    const payment_rule = filesOfProject()
+      .inFolder('payment')
+      .should()
+      .beFreeOfCycles();
+
+    const subscriber_rule = filesOfProject()
+      .inFolder('subscriber')
+      .should()
+      .beFreeOfCycles();
+
+    const subscription_rule = filesOfProject()
+      .inFolder('subscription')
+      .should()
+      .beFreeOfCycles();
+
+    expect(auth_rule).toPassAsync();
+    expect(catalog_rule).toPassAsync();
+    expect(company_rule).toPassAsync();
+    expect(payment_rule).toPassAsync();
+    expect(subscriber_rule).toPassAsync();
+    expect(subscription_rule).toPassAsync();
+  });
 });

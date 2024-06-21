391dbe7c785905ada63b4dca60f01277db2f7496
Hermerson Araújo <ne.hermerson@gmail.com>
2024-06-21 16:14:20 -0300

    refactor: improve modules logic

--- applications/core/src/Server.ts
+++ applications/core/src/Server.ts
@@ -10,6 +10,7 @@ export default class Server {
   constructor() {
     this.#container = this.setupModules();
     const server = new InversifyExpressServer(this.#container);
+
     server.setConfig((app) => {
       app.use(express.json());
       app.use(express.urlencoded({ extended: true }));
@@ -27,9 +28,12 @@ export default class Server {
   }
 
   private setupModules() {
-    const auth_module = AuthModule.init();
     const container = new Container();
-    container.load(auth_module);
+    const modules = [AuthModule];
+    for (let idx = 0; idx < modules.length; idx++) {
+      const module = modules[idx].init();
+      container.load(module);
+    }
     return container;
   }
 }

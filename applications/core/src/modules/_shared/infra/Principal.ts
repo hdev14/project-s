e87b269d2eb613a2e18f8f1f3fb43054ef8f3153
Hermerson Araújo <ne.hermerson@gmail.com>
2024-07-04 22:51:40 -0300

    chore(core): create policies enum

--- applications/core/src/modules/_shared/infra/Principal.ts
+++ applications/core/src/modules/_shared/infra/Principal.ts
@@ -1,6 +1,21 @@
 import { UserObject } from "@auth/domain/User";
 import { interfaces } from "inversify-express-utils";
 
+export enum Policies {
+  /* auth */
+  CREATE_TENANT_USER = 'create_tenant_user',
+  LIST_USERS = 'list_users',
+  UPDATE_USER = 'update_user',
+  UPDATE_USER_POLICIES = 'update_user_policies',
+  CHANGE_ACCESS_PLAN = 'change_access_plan',
+  CREATE_ACCESS_PLAN = 'create_access_plan',
+  UPDATE_ACCESS_PLAN = 'update_access_plan',
+  LIST_POLICIES = 'list_policies',
+  /* catalog */
+  UPDATE_CATALOG_ITEM = 'update_catalog_item',
+  CREATE_CATALOG_ITEM = 'create_catalog_item',
+  LIST_CATALOG_ITEMS = 'list_catalog_items',
+}
 
 export class Principal implements interfaces.Principal {
   details: UserObject | null;

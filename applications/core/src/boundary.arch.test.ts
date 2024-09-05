3afcf311afe117526c8a667f0554d8dfb1ca1941
Hermerson Araújo <ne.hermerson@gmail.com>
2024-09-04 22:11:53 -0300

    test(core): improve arch boundary tests

--- applications/core/src/boundary.arch.test.ts
+++ applications/core/src/boundary.arch.test.ts
@@ -1,67 +1,78 @@
 import "tsarch/dist/jest";
 
+import path from "path";
 import { filesOfProject } from "tsarch";
 
 describe("Architecture Boundaries", () => {
   jest.setTimeout(60000);
 
-  it("domain logic should not depends on application logic", async () => {
-    const rule = filesOfProject()
-      .inFolder("domain")
+  const modules = [
+    'auth',
+    'company',
+    'subscriber',
+    'subscription',
+    'payment',
+    'catalog'
+  ];
+
+
+  test.each(modules)("Module(%s): domain logic should not depends on application logic", async (module) => {
+    const rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
+      .inFolder(`${module}/domain`)
       .shouldNot()
       .dependOnFiles()
-      .inFolder("app");
+      .inFolder(`${module}/app`);
 
     await expect(rule).toPassAsync()
   });
 
-  it("domain logic should not depends on infra logic", async () => {
-    const rule = filesOfProject()
-      .inFolder("domain")
+  test.each(modules)("Module(%s): domain logic should not depends on infra logic", async (module) => {
+    const rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
+      .inFolder(`${module}/domain`)
       .shouldNot()
       .dependOnFiles()
-      .inFolder("infra");
+      .inFolder(`${module}/infra`);
 
     await expect(rule).toPassAsync()
   });
 
-  it("application logic should not depends on infra logic", async () => {
-    const rule = filesOfProject()
-      .inFolder("app")
+  test.each(modules)("Module(%s): application logic should not depends on infra logic", async (module) => {
+    const rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
+      .inFolder(`${module}/app`)
       .shouldNot()
       .dependOnFiles()
-      .inFolder("infra");
+      .inFolder(`${module}/infra`);
 
     await expect(rule).toPassAsync()
   });
 
   it('should not have cyclic dependency between modules', async () => {
-    const auth_rule = filesOfProject()
+    const auth_rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
       .inFolder('auth')
       .should()
       .beFreeOfCycles();
 
-    const catalog_rule = filesOfProject()
+    const catalog_rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
       .inFolder('catalog')
       .should()
       .beFreeOfCycles();
 
-    const company_rule = filesOfProject()
+    const company_rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
       .inFolder('company')
       .should()
       .beFreeOfCycles();
 
-    const payment_rule = filesOfProject()
+    const payment_rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
       .inFolder('payment')
       .should()
       .beFreeOfCycles();
 
-    const subscriber_rule = filesOfProject()
+    const subscriber_rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
       .inFolder('subscriber')
       .should()
       .beFreeOfCycles();
 
-    const subscription_rule = filesOfProject()
+    const subscription_rule = filesOfProject(path.resolve(__dirname, '..', 'tsconfig.json'))
       .inFolder('subscription')
       .should()
       .beFreeOfCycles();

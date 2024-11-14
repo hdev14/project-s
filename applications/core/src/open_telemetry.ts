72931eb3ec9be06b83546ffdb672aed074895b46
Hermerson Araújo <ne.hermerson@gmail.com>
2024-11-13 23:32:21 -0300

    chore(otlp): setup open telemetry with jeager and prometheus

--- applications/core/src/open_telemetry.ts
+++ applications/core/src/open_telemetry.ts
@@ -1,17 +1,40 @@
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import {
   ConsoleMetricExporter,
   PeriodicExportingMetricReader,
 } from '@opentelemetry/sdk-metrics';
-import { NodeSDK } from '@opentelemetry/sdk-node';
+import { NodeSDK, NodeSDKConfiguration } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 
-const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(), // TODO: add an external OTLP collect like jeager or prometheus.
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
-  instrumentations: [getNodeAutoInstrumentations()],
-});
+const configuration: Partial<NodeSDKConfiguration> = process.env.NODE_ENV === 'test'
+  ? ({
+    serviceName: 'core',
+    traceExporter: new ConsoleSpanExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new ConsoleMetricExporter(),
+    }),
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+    ],
+  }) : ({
+    serviceName: 'core',
+    traceExporter: new OTLPTraceExporter({
+      url: process.env.JEAGER_TRACING_URL ?? 'http://localhost:4318/v1/traces',
+      headers: {}
+    }),
+    metricReader: new PrometheusExporter({
+      port: 9464,
+    }),
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+    ],
+  });
+
+const sdk = new NodeSDK(configuration);
 
 sdk.start();
